70fed4f9a0216ab9bc31d287d1b81615be325bd8
ritwik <ritwik@ritwik.net>
2018-08-23 20:53:10 +0530

    run headless

--- envs/gym/env_lunar_lander.py
+++ envs/gym/env_lunar_lander.py
@@ -30,18 +30,19 @@ def main(args):
     agent = DqnAgent(env, create_model)
 
     if not args.skip_train:
+        render_every = 100 if not args.headless else None
         avg_rewards, best_avg_reward = Engine.train(env, agent,
                                                     num_episodes=2000,
                                                     max_steps=max_steps,
                                                     solved_avg_reward=200.0,
-                                                    render_every=100)
+                                                    render_every=render_every)
 
         plt.plot(avg_rewards)
         plt.show()
 
         agent.save_model(model_file)
 
-    if not args.skip_test:
+    if not args.headless and not args.skip_test:
         if os.path.exists(model_file):
             agent.load_model(model_file)
         else:
@@ -52,8 +53,9 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='OpenGym Lunar Lander Demo')
-    parser.add_argument('--skip_train', action='store_true', help='Skip training.')
-    parser.add_argument('--skip_test', action='store_true', help='Skip training.')
+    parser.add_argument('--skip_train', action='store_true', help='Skip training')
+    parser.add_argument('--skip_test', action='store_true', help='Skip testing')
+    parser.add_argument('--headless', action='store_true', help='Disable rendering')
     args = parser.parse_args()
 
     main(args)
--- envs/gym/env_taxi.py
+++ envs/gym/env_taxi.py
@@ -19,14 +19,15 @@ def main(args):
         plt.ylim([min(0, np.max(avg_rewards)), 10])
         plt.show()
 
-    if not args.skip_test:
+    if not args.headless and not args.skip_test:
         Engine.test(env, agent, num_episodes=1, mode='human')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='OpenGym Taxi Demo')
-    parser.add_argument('--skip_train', action='store_true', help='Skip training.')
-    parser.add_argument('--skip_test', action='store_true', help='Skip training.')
+    parser.add_argument('--skip_train', action='store_true', help='Skip training')
+    parser.add_argument('--skip_test', action='store_true', help='Skip testing')
+    parser.add_argument('--headless', action='store_true', help='Disable rendering')
     args = parser.parse_args()
 
     main(args)
